b945d050d7c55b8e251e0c3f28c161b9173bdb76
Adam Howes <howesadam@live.co.uk>
2020-01-01 12:05:22 +0000

    Added location permissions

--- app/src/main/java/com/example/whenweather/SplashScreen.kt
+++ app/src/main/java/com/example/whenweather/SplashScreen.kt
@@ -1,25 +1,49 @@
 package com.example.whenweather
 
-import android.app.Activity
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import java.util.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
 
 class SplashScreen : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
         supportActionBar?.hide()
 
-        Thread.sleep(1000)
+        val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        ActivityCompat.requestPermissions(this, permissions, 0)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is not granted
+            // Todo: add response if permission is denied
+        }
+
+        //Gets the location of the device
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // Got last known location. In some rare situations this can be null.
+            }
 
         newActivity()
     }
 
-    fun newActivity(){
-        startActivity(Intent(this@SplashScreen,MainScreen::class.java))
+    private fun newActivity() {
+        startActivity(Intent(this@SplashScreen, MainScreen::class.java))
     }
 }
